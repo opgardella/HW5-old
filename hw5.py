fbfedcbe10b8169ec89e0643b718c7ea37047256
Olivia Gardella <gardella@umich.edu>
2017-10-07 09:59:40 -0400

    hw5 complete

--- hw5.py
+++ hw5.py
@@ -1,2 +1,18 @@
 #HW5
 #Olivia Gardella
+
+import re
+
+file = open('regex_sum_35643.txt')
+list = []
+
+for line in file:
+    line = line.rstrip()
+    words = re.findall('([0-9]+)', line)
+    value = sum(map(int, words))
+    list.append(value)
+
+count = 0
+for num in list:
+    count += num
+print(count)
